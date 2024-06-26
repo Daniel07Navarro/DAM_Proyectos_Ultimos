079b644331fb05e1584dc9acd03cab8b3f8f161a
Daniel07Navarro <Daniel7Navarro7@gmail.com>
2024-06-26 12:15:12 -0500

    Agregando semana 15

---
+++ DAM_SAM15_Project/app/src/main/java/com/example/dam_sem11_proyecto/db/NotaRoomDatabase.java
@@ -0,0 +1,29 @@
+package com.example.dam_sem11_proyecto.db;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+
+import com.example.dam_sem11_proyecto.db.dao.NotaDao;
+import com.example.dam_sem11_proyecto.db.entity.NotaEntity;
+
+@Database(entities = {NotaEntity.class}, version = 1)
+public abstract class NotaRoomDatabase {
+
+    public abstract NotaDao notaDao();
+    private static volatile NotaRoomDatabase INSTANCE;
+
+    public static NotaRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (NotaRoomDatabase.class){
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(), NotaRoomDatabase.class, "notas_database").build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+}
+
+
